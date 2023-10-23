4debc4df40dd0da6ba8e8015d634b969838f6875
Michael Vilain <michael@vilain.com>
2023-10-23 03:46:54 -0700

    add Opera support to update-bookmarks

--- update-bookmarks.sh
+++ update-bookmarks.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # update Safari and Brave bookmarks files from file
+# 11/23/23 add Opera support
 
 SCRIPT=$(basename "$0")
 hosts="studio dexlabs dede"
@@ -7,23 +8,26 @@ SAFARI_DIR='~/Library/Safari/'
 OPT_S=~/Desktop/Bookmarks.plist
 BRAVE_DIR='~/Library/ApplicationSupport/BraveSoftware/Brave-Browser/Default/'
 OPT_B=~/Desktop/Bookmarks
+OPERA_DIR='/Users/vilain/Library/ApplicationSupport/com.operasoftware.Opera/'
+OPT_O=~/Desktop/Bookmarks
 
 usage () {
     cat <<-USAGE
 
-    ${SCRIPT} -h -b <BRAVE_FILE> -s <SAFARI_FILE>
+    ${SCRIPT} -h -b <BRAVE_FILE> -s <SAFARI_FILE> -o <OPERA_FILE>
 
     copy file to browser's bookmarks (format different for each browser)
 
     OPTIONS
     -s FILE    copy FILE to Safari Bookmarks.plist [default: ${OPT_S}]
     -b FILE    copy FILE to Brave Bookmarks [default: ${OPT_B}]
+    -o FILE    copy FILE to Opera Bookmarks [default: ${OP_O}]
     -h         display this help text and exit
 
 USAGE
 }
 
-while getopts ":hb:s:" OPT; do
+while getopts ":hb:s:o:" OPT; do
   case $OPT in
   h)
     usage
@@ -31,6 +35,9 @@ while getopts ":hb:s:" OPT; do
     ;;
   b)
     OPT_B=${OPTARG}
+    ;;
+  o)
+    OPT_O=${OPTARG}
     ;;
   s)
     OPT_S=${OPTARG}
@@ -45,20 +52,26 @@ done
 shift $((OPTIND-1))
 # $@ should be blank...it is ignored
 
-if [[ -z "${OPT_B}" || -z "${OPT_S}" ]]; then
+if [[ -z "${OPT_B}" || -z "${OPT_S}" || -z "${OPT_O}" ]]; then
   usage
   exit 2
 fi
 
 if [ -e "${OPT_B}" ]; then
   for h in ${hosts}; do
-    printf "%s...\n" $h
+    printf "Brave %s...\n" $h
     scp ${OPT_B} ${h}:${BRAVE_DIR}
+  done
+fi
+if [ -e "${OPT_O}" ]; then
+  for h in ${hosts}; do
+    printf "Opera %s...\n" $h
+    scp ${OPT_O} ${h}:${OPERA_DIR}
   done
 fi
 if [ -e "${OPT_S}" ]; then
   for h in ${hosts}; do
-    printf "%s...\n" $h
+    printf "Safari %s...\n" $h
     scp ${OPT_S} ${h}:${SAFARI_DIR}
   done
 fi
