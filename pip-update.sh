b7397c4b1c742b2b1c0b370d3d7f3bb21d62366d
Michael Vilain <michael@vilain.com>
2025-07-11 15:25:53 -0700

    add ansible to 3.9-3.11; remove brew managed packages from 3.12-3.13

--- pip-update.sh
+++ pip-update.sh
@@ -10,6 +10,7 @@
 # added pynvim 202405.30
 # added python3.13 2412.25
 # removed setuptools from 3.12+3.13 as brew installs it
+# moved python 3.12+3.13 to install into pipx as it won't allow updates 202507.11
 
 [ $EUID -ne 0 ] && echo "not root" && exit
 
@@ -17,141 +18,61 @@
 # echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> $(date) python2.7"
 # echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 # python2.7 -m pip install --upgrade \
-#   altgraph \
-#   astroid \
-#   backports.ssl-match-hostname \
-#   bdist-mpkg \
-#   certifi \
-#   contextlib2 \
-#   cycler \
+#   altgraph astroid \
+#   backports.ssl-match-hostname bdist-mpkg \
+#   certifi contextlib2 cycler \
 #   docker-pycreds \
-#   entrypoints \
-#   enum34 \
-#   funcsigs \
-#   functools32 \
-#   importlib-metadata \
-#   ipaddress \
+#   entrypoints enum34 \
+#   funcsigs functools32 \
+#   importlib-metadata ipaddress \
 #   kiwisolver \
 #   List \
-#   macholib \
-#   matplotlib \
-#   mccabe \
-#   modulegraph \
+#   macholib matplotlib mccabe modulegraph \
 #   numpy \
-#   pip \
-#   py2app \
-#   pyparsing \
-#   pytz \
-#   scandir \
-#   scipy \
-#   setuptools \
-#   singledispatch \
-#   six \
-#   subprocess32 \
-#   tqdm \
-#   twine \
-#   typing \
-#   webencodings \
-#   websocket-client \
-#   wheel \
-#   wrapt \
+#   pip py2app pyparsing pytz \
+#   scandir scipy setuptools singledispatch six subprocess32 \
+#   tqdm twine typing \
+#   webencodings websocket-client wheel wrapt \
 #   xattr \
 #   yYAML \
-#   zipp \
-#   zope.interface
+#   zipp zope.interface
 
-for ver in python3.9; do
+for ver in python3.9 python3.10 python3.11; do
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> $(date) ${ver}"
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     ${ver} -m pip install --upgrade \
-        asn1crypto \
-        astroid \
-        certifi \
-        cffi \
-        dill \
+        ansible ansible-core ansible-lint \
+        asn1crypto astroid \
+        certifi cffi \
         entrypoints \
-        filelock \
         flake8 \
-        importlib-metadata \
-        isort \
+        importlib-metadata isort \
         lazy-object-proxy \
         mccabe \
-        packaging \
         pip \
-        platformdirs \
-        protobuf \
-        pycodestyle \
-        pycparser \
-        pyflakes \
-        pylint \
-        pynvim \
-        pytest \
-        PyYAML \
-        setuptools \
-        six \
-        toml \
-        tomli \
-        typed-ast \
-        typing-extensions \
+        platformdirs protobuf pycodestyle pycparser pyflakes pylint pynvim PyYAML \
+        setuptools six Sphinx sphinxcontrib-napoleon \
+        toml tomli typed-ast typing-extensions \
         urllib3 \
         virtualenv \
-        wheel \
-        wrapt \
-        yamllint \
+        wheel wrapt \
         xattr \
+        yamllint \
         zipp
-#    ${ver} -m pip uninstall -y dill distlib iniconfig pathspec py pyrsistent pytest PyYAML tomli tomlkit attrs filelock pluggy
 done
 
 # 2/16/22 installed ansible...generated with
 # # python3.9 -m pip list | awk '!/Package|-----/{printf "\t\t%s \\\n",$1}'
 
-ver=python3.10
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> $(date) ${ver}"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-${ver} -m pip install --upgrade \
-    ansible ansible-core ansible-lint \
-    asn1crypto \
-    astroid \
-    certifi \
-    cffi \
-    entrypoints \
-    flake8 \
-    importlib-metadata \
-    isort \
-    lazy-object-proxy \
-    mccabe \
-    pip \
-    platformdirs \
-    protobuf \
-    pycodestyle \
-    pycparser \
-    pyflakes \
-    pylint \
-    pynvim \
-    PyYAML \
-    setuptools \
-    Sphinx sphinxcontrib-napoleon \
-    six \
-    toml \
-    tomli \
-    typed-ast \
-    typing-extensions \
-    urllib3 \
-    virtualenv \
-    wheel \
-    wrapt \
-    yamllint \
-    xattr \
-    zipp
-
-for ver in python3.11 python3.12 python3.13; do
-  echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-  echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> $(date) ${ver}"
-  echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-  ${ver} -m pip install --upgrade --break-system-packages certifi pip PyYAML six wheel virtualenv
+# brew maintains certifi and python-setuptools for python 3.12 or 3.13
+# but still set ownership of site-packages; 202507.11
+for ver in python3.12 python3.13; do
+#   echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+#   echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> $(date) ${ver} pipx"
+#   echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+#   brew upgrade certifi python-setuptools
+  ${ver} -m pip install --upgrade --break-system-packages filelock pip PyYAML six wheel virtualenv
   for d in /opt/homebrew/lib/${ver}/site-packages /usr/local/lib/${ver}/site-packages; do
     [ -e ${d} ] && sudo chown -R vilain:staff ${d}
   done
