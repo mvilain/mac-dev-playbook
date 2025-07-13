98d2ac8c2b15faa3d4739ee71774d13d41b6daeb
Michael Vilain <michael@vilain.com>
2025-07-13 06:52:50 -0700

    group packages alphabetically; assumes setuptools, certifi, virtualenv installed as root

--- pip-update-zorin.sh
+++ pip-update-zorin.sh
@@ -2,6 +2,7 @@
 # update zorin's python3 libraries to local environment 202407.03MeV
 # remove duplicity which now uses pipx 202408.05MeV
 # remove testresources which has install bug 202505.03MeV
+# assume setuptools certifi virtualenv installed in root install 202507.13MeV
 
 # should NOT be run as root; should update ~/.local/bin instead
 [ $EUID -eq 0 ] && echo "should not be run as root" && exit
@@ -16,121 +17,39 @@ for ver in /usr/bin/python3; do
     source ~/venv/bin/activate
     ${ver} -m pip install --upgrade \
      ansible ansible-base ansible-core ansible-lint \
-     apache-libcloud \
-     apturl \
-     argcomplete \
-     asn1crypto \
-     astroid \
-     Babel \
-     bcrypt \
-     blinker \
-     bracex \
-     Brlapi \
-     cachetools \
-     certifi \
-     cffi \
-     chardet \
-     click \
-     colorama \
-     command-not-found \
-     commonmark \
-     cryptography \
-     cupshelpers \
-     defer \
-     distlib \
-     distro \
-     distro-info \
-     dnspython \
-     enrich \
-     entrypoints \
-     fasteners \
-     filelock \
-     flake8 \
-     folder-color \
-     future \
-     google-api-python-client google-auth google-auth-httplib2 \
-     greenlet \
+     apache-libcloud apturl argcomplete asn1crypto astroid \
+     Babel bcrypt blinker bracex Brlapi \
+     cachetools certifi cffi chardet click colorama \
+     command-not-found commonmark cryptography cupshelpers \
+     defer distlib distro distro-info dnspython \
+     enrich entrypoints \
+     fasteners filelock flake8 folder-color future \
+     google-api-python-client google-auth google-auth-httplib2 greenlet \
      httplib2 \
-     icoextract \
-     idna \
-     importlib-metadata \
-     isort \
-     jeepney \
-     Jinja2 \
-     jmespath \
+     icoextract idna importlib-metadata isort \
+     jeepney Jinja2 jmespath \
      keyring \
-     language-selector \
-     launchpadlib \
-     lazr.restfulclient \
-     lazr.uri \
-     lazy-object-proxy \
-     lockfile \
-     logilab-common \
-     louis \
-     macaroonbakery \
-     Mako \
-     MarkupSafe \
-     mccabe \
-     monotonic \
-     more-itertools \
-     msgpack \
-     mypy-extensions \
-     netaddr \
-     netifaces \
-     ntlm-auth \
-     oauth2client \
-     oauthlib \
-     olefile \
-     packaging \
-     paramiko \
-     pathspec \
-     "pbr<2.0" \
-     pefile \
-     pexpect \
-     Pillow \
-     platformdirs \
-     protobuf \
-     psutil \
-     ptyprocess \
+     language-selector launchpadlib lazr.restfulclient lazr.uri lazy-object-proxy \
+     lockfile logilab-common louis \
+     macaroonbakery Mako MarkupSafe mccabe monotonic more-itertools msgpack mypy-extensions \
+     netaddr netifaces ntlm-auth \
+     oauth2client oauthlib olefile \
+     packaging paramiko pathspec "pbr<2.0" pefile pexpect Pillow \
+     platformdirs protobuf psutil ptyprocess \
      pyasn1 pyasn1-modules pycodestyle pycparser pycryptodomex \
      PyDrive pyelftools pyflakes Pygments PyJWT pykerberos pylint \
      pymacaroons PyNaCl pynvim pyOpenSSL pyparsing pyRFC3339 python-dateutil \
      python-vagrant pytz pywinrm pyxdg PyYAML \
-     reportlab \
-     requests \
-     requests-kerberos \
-     requests-ntlm \
-     requests-toolbelt \
-     resolvelib \
-     rich \
-     rsa \
+     reportlab requests requests-kerberos requests-ntlm \
+     requests-toolbelt resolvelib rich rsa \
      ruamel.yaml ruamel.yaml.clib \
-     SecretStorage \
-     selinux \
-     setuptools \
-     simplejson \
-     six \
-     Sphinx sphinxcontrib-napoleon \
-     SQLAlchemy \
-     ssh-import-id \
-     tenacity \
-     toml \
-     typed-ast \
-     typing-extensions \
-     ubuntu-drivers-common \
-     ufw \
-     unattended-upgrades \
-     uritemplate \
-     urllib3 \
+     SecretStorage selinux setuptools simplejson six \
+     Sphinx sphinxcontrib-napoleon SQLAlchemy ssh-import-id \
+     tenacity toml typed-ast typing-extensions \
+     ubuntu-drivers-common ufw unattended-upgrades uritemplate urllib3 \
      vboxapi \
-     wadllib \
-     wcmatch \
-     wheel \
-     wrapt \
-     xattr \
-     xdg \
-     xkit \
-     xmltodict \
+     wadllib wcmatch wheel wrapt \
+     xattr xdg xkit xmltodict \
      yamllint \
      zipp
      #dbus-python \
