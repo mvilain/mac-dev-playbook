5fdf2dd608218c39f6ac07d01fff8465061f1da8
Michael Vilain <michael@vilain.com>
2025-07-13 07:26:22 -0700

    change pip-update-zorin to use python3.13; remove python3.13 dependencies

--- pip-update-zorin.sh
+++ pip-update-zorin.sh
@@ -2,12 +2,12 @@
 # update zorin's python3 libraries to local environment 202407.03MeV
 # remove duplicity which now uses pipx 202408.05MeV
 # remove testresources which has install bug 202505.03MeV
-# assume setuptools certifi virtualenv installed in root install 202507.13MeV
+# assume setuptools certifi virtualenv installed in root install; runs 3.13 202507.13MeV
 
 # should NOT be run as root; should update ~/.local/bin instead
 [ $EUID -eq 0 ] && echo "should not be run as root" && exit
 
-for ver in /usr/bin/python3; do
+for ver in /usr/local/bin/python3.13; do
   if [ -e $ver ]; then
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> $(date) ${ver}"
@@ -17,19 +17,18 @@ for ver in /usr/bin/python3; do
     source ~/venv/bin/activate
     ${ver} -m pip install --upgrade \
      ansible ansible-base ansible-core ansible-lint \
-     apache-libcloud apturl argcomplete asn1crypto astroid \
-     Babel bcrypt blinker bracex Brlapi \
-     cachetools certifi cffi chardet click colorama \
-     command-not-found commonmark cryptography cupshelpers \
+     apache-libcloud argcomplete asn1crypto astroid \
+     Babel bcrypt blinker bracex \
+     cachetools certifi cffi chardet click colorama commonmark cryptography \
      defer distlib distro distro-info dnspython \
      enrich entrypoints \
-     fasteners filelock flake8 folder-color future \
+     fasteners filelock flake8 future \
      google-api-python-client google-auth google-auth-httplib2 greenlet \
      httplib2 \
      icoextract idna importlib-metadata isort \
      jeepney Jinja2 jmespath \
      keyring \
-     language-selector launchpadlib lazr.restfulclient lazr.uri lazy-object-proxy \
+     launchpadlib lazr.restfulclient lazr.uri lazy-object-proxy \
      lockfile logilab-common louis \
      macaroonbakery Mako MarkupSafe mccabe monotonic more-itertools msgpack mypy-extensions \
      netaddr netifaces ntlm-auth \
@@ -45,18 +44,16 @@ for ver in /usr/bin/python3; do
      ruamel.yaml ruamel.yaml.clib \
      SecretStorage selinux setuptools simplejson six \
      Sphinx sphinxcontrib-napoleon SQLAlchemy ssh-import-id \
-     tenacity toml typed-ast typing-extensions \
-     ubuntu-drivers-common ufw unattended-upgrades uritemplate urllib3 \
-     vboxapi \
+     tenacity toml typing-extensions \
+     uritemplate urllib3 \
      wadllib wcmatch wheel wrapt \
-     xattr xdg xkit xmltodict \
+     xattr xdg xmltodict \
      yamllint \
      zipp
-     #dbus-python \
-     #python-apt \
-     #python-debian \
-     #zorin-appearance \
-     #zorin-connect \
-     #zorin-exec-guard
+
+     #dbus-python python-apt python-debian 202407.03MeV
+     #zorin-appearance zorin-connect zorin-exec-guard 202407.03MeV
+     # apturl Brlapi command-not-found cupshelpers folder-color language-selector 202507.13
+     # typed-ast ubuntu-drivers-common ufw unattended-upgrades vboxapi xkit 202507.13MeV
   fi
 done
